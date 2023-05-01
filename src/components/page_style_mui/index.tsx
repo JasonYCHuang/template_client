aad353fe3247c87cc98715f7b87f1124d5276cc0
JasonYCHuang <jasonych99@gmail.com>
2023-05-02 00:27:54 +0800

    add style-mui

--- src/components/page_style_mui/index.tsx
+++ src/components/page_style_mui/index.tsx
@@ -1,7 +1,44 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+
+const styleScreen = {
+  backgroundColor: '#ddd',
+  padding: '10px 30px 10px 30px',
+};
+
+const CalcByBtnComp = () => (
+  <Stack spacing={2} direction="row">
+    <Button variant="contained" startIcon={<RemoveCircleOutlineIcon />}>
+      -1
+    </Button>
+    <Typography variant="h5" style={styleScreen}>
+      0
+    </Typography>
+    <Button variant="outlined" startIcon={<AddCircleOutlineIcon />}>
+      +1
+    </Button>
+  </Stack>
+);
+
+const AddByInputComp = () => (
+  <Stack spacing={2} direction="row">
+    <TextField label="輸入" variant="outlined" />
+    <Button variant="contained" color="success" startIcon={<AutoFixHighIcon />} />
+  </Stack>
+);
+
 const PageStyleMui = () => (
-  <div>
-    <p>Hello World... Mui</p>
-  </div>
+  <Box>
+    <CalcByBtnComp />
+    <br />
+    <AddByInputComp />
+  </Box>
 );
 
 export default PageStyleMui;
