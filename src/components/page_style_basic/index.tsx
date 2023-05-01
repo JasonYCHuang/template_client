1de9dc7b9987ac3bb69596d690390a362a17d80b
JasonYCHuang <jasonych99@gmail.com>
2023-05-01 23:58:12 +0800

    add style-basic

--- src/components/page_style_basic/index.tsx
+++ src/components/page_style_basic/index.tsx
@@ -1,6 +1,38 @@
+const styleRoot = {
+  border: '1px solid red',
+  borderRadius: '3px',
+};
+
+const styleContainer1 = {
+  border: '2px solid #00ff00',
+  borderRadius: '10px',
+  margin: '5px 10px 20px 30px',
+};
+
+const styleContainer2 = {
+  backgroundColor: '#ddd',
+  border: '2px solid blue',
+  borderRadius: '3px',
+};
+
+const Container1Comp = () => {
+  const text = 'Hello World... Basic 01';
+
+  return (
+    <p>
+      { text }
+    </p>
+  );
+};
+
 const PageStyleBasic = () => (
-  <div>
-    <p>Hello World... Basic</p>
+  <div style={styleRoot}>
+    <div style={styleContainer1}>
+      { Container1Comp() }
+    </div>
+    <div style={styleContainer2}>
+      <p>Hello World... Basic 02</p>
+    </div>
   </div>
 );
 
