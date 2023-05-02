6c36e063dfc16ec14c8289e7c8cdaff06f6bb0e6
Jason YC Huang <jasonych99@gmail.com>
2023-05-02 09:03:06 +0800

    use data-reducer 06 : split into files

--- src/components/page_data_reducer/index.tsx
+++ src/components/page_data_reducer/index.tsx
@@ -1,85 +1,9 @@
-import { useRef } from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
-import { useAppDispatch, useAppSelector } from '../../reducers/hooks';
-import { plusNum, minusNum } from '../../actions/calculatorSlice';
+import CalcByBtnComp from './comp_calc_by_btn';
+import AddByInputComp from './comp_add_by_input';
 
 const styleRoot = {
   border: '1px solid red',
-};
-
-const styleScreen = {
-  backgroundColor: '#ddd',
-  padding: '10px 30px 10px 30px',
-};
-
-const delta = 5;
-
-const CalcByBtnComp = () => {
-  const dispatch = useAppDispatch();
-  const { result } = useAppSelector((state) => state.calculator);
-
-  const onPlusNum = (num: number) => {
-    dispatch(plusNum(num));
-  };
-  const onMinusNum = (num: number) => {
-    dispatch(minusNum(num));
-  };
-
-  return (
-    <Stack spacing={2} direction="row">
-      <Button
-        variant="contained"
-        startIcon={<RemoveCircleOutlineIcon />}
-        onClick={() => onMinusNum(delta)}
-      >
-        -
-      </Button>
-      <Typography variant="h5" style={styleScreen}>
-        { result }
-      </Typography>
-      <Button
-        variant="outlined"
-        startIcon={<AddCircleOutlineIcon />}
-        onClick={() => onPlusNum(delta)}
-      >
-        +
-      </Button>
-    </Stack>
-  );
-};
-
-const AddByInputComp = () => {
-  const dispatch = useAppDispatch();
-
-  const fieldRef = useRef<HTMLInputElement>();
-  const onAddByInput = () => {
-    const { value: fieldVal } = fieldRef.current || { value: '' };
-    const intFieldVal = parseInt(fieldVal, 10) || 0;
-    dispatch(plusNum(intFieldVal));
-  };
-
-  return (
-    <Stack spacing={2} direction="row">
-      <TextField
-        label="輸入"
-        variant="outlined"
-        inputRef={fieldRef}
-      />
-      <Button
-        variant="contained"
-        color="success"
-        startIcon={<AutoFixHighIcon />}
-        onClick={onAddByInput}
-      />
-    </Stack>
-  );
 };
 
 const PageStyleMui = () => (
