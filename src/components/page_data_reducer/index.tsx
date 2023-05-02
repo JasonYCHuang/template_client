84f25ffa9e83d4eb00f8ccb5a1d729862dbe10d2
Jason YC Huang <jasonych99@gmail.com>
2023-05-02 08:59:24 +0800

    use data-reducer 05 : useRef

--- src/components/page_data_reducer/index.tsx
+++ src/components/page_data_reducer/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
@@ -54,12 +55,32 @@ const CalcByBtnComp = () => {
   );
 };
 
-const AddByInputComp = () => (
-  <Stack spacing={2} direction="row">
-    <TextField label="輸入" variant="outlined" />
-    <Button variant="contained" color="success" startIcon={<AutoFixHighIcon />} />
-  </Stack>
-);
+const AddByInputComp = () => {
+  const dispatch = useAppDispatch();
+
+  const fieldRef = useRef<HTMLInputElement>();
+  const onAddByInput = () => {
+    const { value: fieldVal } = fieldRef.current || { value: '' };
+    const intFieldVal = parseInt(fieldVal, 10) || 0;
+    dispatch(plusNum(intFieldVal));
+  };
+
+  return (
+    <Stack spacing={2} direction="row">
+      <TextField
+        label="輸入"
+        variant="outlined"
+        inputRef={fieldRef}
+      />
+      <Button
+        variant="contained"
+        color="success"
+        startIcon={<AutoFixHighIcon />}
+        onClick={onAddByInput}
+      />
+    </Stack>
+  );
+};
 
 const PageStyleMui = () => (
   <Box style={styleRoot}>
