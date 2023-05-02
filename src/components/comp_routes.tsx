5995e5e71c8f319d101c0e09bcb863dfac900974
Jason YC Huang <jasonych99@gmail.com>
2023-05-02 11:33:38 +0800

    modify routes A

--- src/components/comp_routes.tsx
+++ src/components/comp_routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import PageStyleBasic from './page_style_basic/index';
 import PageStyleMui from './page_style_mui/index';
+import PageStyleMuiAdv from './page_style_mui_adv/index';
 import PageDataReducer from './page_data_reducer/index';
 
 const RoutesComp = () => (
@@ -10,6 +11,7 @@ const RoutesComp = () => (
       <Route path="/" element={<PageStyleBasic />} />
       <Route path="/style_basic" element={<PageStyleBasic />} />
       <Route path="/style_mui" element={<PageStyleMui />} />
+      <Route path="/style_mui_adv" element={<PageStyleMuiAdv />} />
       <Route path="/data_reducer" element={<PageDataReducer />} />
     </Routes>
   </Box>
